c757b08754a1adfcc725f2bd385d422535081fca
phamvanlinh20111993 <phamvanlinh20111993@gmail.com>
2018-07-15 19:49:09 +0700

    bracket

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -1,6 +1,12 @@
 
-app.controller('homeCtrl',['$scope',
-   function ($scope){
-    $scope.tagline = 'To the moon and back!';   
+app.controller('homeCtrl',['$scope','$rootScope', '$cookies',
+   function ($scope, $rootScope, $cookies){
+    	$scope.tagline = 'To the moon and back!'; 
+
+    //	console.log($scope.get($scope.bracket))  
+
+    	$scope.addToBracket = function(id){
+    		 $rootScope.bracket++;
+    	}
    }
 ]);
